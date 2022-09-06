78cee761765cad71ab9e212b1c1e159890b42c53
sivalgabe <sival.gabriel@gmail.com>
2022-09-06 16:23:18 -0300

    duas activitys completas, sistema de database implementado, falta exibição

--- app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
+++ app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
@@ -1,10 +1,15 @@
 package br.com.igti.frases.ui.incluirfrase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import br.com.igti.frases.R
+import br.com.igti.frases.data.Frase
 import br.com.igti.frases.databinding.ActivityIncluirFraseBinding
 import br.com.igti.frases.databinding.ActivityMainBinding
+import br.com.igti.frases.ui.main.MainActivity
 
 class IncluirFraseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIncluirFraseBinding
@@ -41,6 +46,20 @@ class IncluirFraseActivity : AppCompatActivity() {
             binding.FraseTIL.error = getString(R.string.tvErrorSemFrase)
         } else {
             binding.FraseTIL.error = null
+        }
+
+        if (autor.isNotEmpty() && frase.isNotEmpty()) {
+            Log.i("IGTIinfo","Autor: $autor, Frase: $frase")
+            Intent().apply {
+                putExtra(MainActivity.RETORNO, Frase(
+                    autor = autor,
+                    frase = frase
+                ))
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        } else {
+            Log.i("IGTIinfo","Dados não Salvos, os campos estavam vazios")
         }
     }
 
--- app/src/main/java/br/com/igti/frases/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/igti/frases/ui/main/MainActivity.kt
@@ -3,11 +3,28 @@ package br.com.igti.frases.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import br.com.igti.frases.data.Frase
 import br.com.igti.frases.databinding.ActivityMainBinding
 import br.com.igti.frases.ui.incluirfrase.IncluirFraseActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private val retornoFrase = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { activityResult ->
+        if ( activityResult.resultCode == RESULT_OK) {
+            activityResult.data?.let {
+                if (it.hasExtra(RETORNO)){
+                    Log.i("IGTIinfo", "Retorno: ${it.getParcelableExtra<Frase>(RETORNO)}")
+                }
+            }
+        } else {
+            Log.e("IGTIerror:","Não foi possível obter os dados de retorno da activity.")
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,8 +40,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun configFabListeners() {
         binding.fabAddNovaFrase.setOnClickListener{
-            val intent = Intent(this, IncluirFraseActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this, IncluirFraseActivity::class.java).let {
+                retornoFrase.launch(it)
+            }
         }
+    }
+
+    companion object {
+        const val RETORNO = "retorno_frases"
     }
 }
