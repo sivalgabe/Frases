3dcf30fedd30de7256d44df6a40627a6e5e2d589
sivalgabe <sival.gabriel@gmail.com>
2022-09-06 11:07:26 -0300

    criação do layout da activity inicial

--- app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
+++ app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
@@ -1,11 +1,12 @@
-package br.com.igti.frases
+package br.com.igti.frases.ui.incluirfrase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.igti.frases.R
 
-class MainActivity : AppCompatActivity() {
+class IncluirFraseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_incluir_frase)
     }
 }
---
+++ app/src/main/java/br/com/igti/frases/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.igti.frases.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.igti.frases.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
