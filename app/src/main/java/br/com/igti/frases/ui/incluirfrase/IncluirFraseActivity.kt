7f7cb1ab06c76af601616f0c435795100c1951e7
sivalgabe <sival.gabriel@gmail.com>
2022-09-06 13:48:21 -0300

    função ao botão cancelar

--- app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
+++ app/src/main/java/br/com/igti/frases/ui/incluirfrase/IncluirFraseActivity.kt
@@ -3,10 +3,26 @@ package br.com.igti.frases.ui.incluirfrase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.igti.frases.R
+import br.com.igti.frases.databinding.ActivityIncluirFraseBinding
+import br.com.igti.frases.databinding.ActivityMainBinding
 
 class IncluirFraseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIncluirFraseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_incluir_frase)
+        binding = ActivityIncluirFraseBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configListeners()
+    }
+
+    private fun configListeners() {
+        configListenersBtnCancelar()
+    }
+
+    private fun configListenersBtnCancelar() {
+        binding.btnCancelar.setOnClickListener{
+            finish()
+        }
     }
 }
--- app/src/main/java/br/com/igti/frases/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/igti/frases/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.com.igti.frases.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.igti.frases.databinding.ActivityMainBinding
+import br.com.igti.frases.ui.incluirfrase.IncluirFraseActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -11,5 +13,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        configListeners()
+    }
+
+    private fun configListeners() {
+        configFabListeners()
+    }
+
+    private fun configFabListeners() {
+        binding.fabAddNovaFrase.setOnClickListener{
+            val intent = Intent (this, IncluirFraseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
